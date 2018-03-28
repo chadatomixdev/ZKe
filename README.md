21e200ac773465a0e2dabc646233039a9ac33841
Chad Bonthuys <chad@freshive.co.za>
2018-03-28 16:31:34 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ZKe
+
+Test POC for .Net Standard Library for Stack Overflow Question
---
+++ ZkemDLLFix/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using zkemkeeper;
+using ZkemDLLFix.Models;
+
+namespace ZkemDLLFix.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+            var c = new CZKEMClass();
+        }
+          
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
